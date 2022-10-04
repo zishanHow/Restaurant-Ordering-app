e023cbd40ff646ee1f50245b629d453a11a58aa6
zishanHow <111298322+zishanHow@users.noreply.github.com>
2022-10-05 03:54:06 +0600

    update index.js
    make the code dynamic with javascript.
    rendering the array of object with javaschipt forEach loop.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-import { menuArray } from "./data.js" 
+import { menuArray } from "./data.js"
 
-console.log(menuArray)
+const main = document.getElementById("main")
+// console.log(main)
+
+function getRestaurantHtml() {
+    let restaurantHtml = ""
+    menuArray.forEach(function (restaurant) {
+        restaurantHtml += `
+        <section>
+            <div class="item-flex">
+                <div class="item">${restaurant.emoji}</div>
+                <div class="item-details">
+                        <h3 class="item-header">${restaurant.name}</h3>
+                        <p class="about-item">${restaurant.ingredients}</p>
+                        <p class="item-price">$${restaurant.price}</p>
+                </div>
+            </div>
+
+            <div class="add-icon"><i class="fa-solid fa-plus"></i></div>
+        </section>
+        `
+    })
+    main.innerHTML = restaurantHtml
+}
+
+getRestaurantHtml()
